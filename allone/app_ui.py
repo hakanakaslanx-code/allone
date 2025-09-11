595e89b5b07bef3a4996257a6c42f218ddfb5c36
hakanakaslanx-code <hakanakaslanx@gmail.com>
2025-09-11 17:04:15 -0400

    Update app_ui.py

--- allone/app_ui.py
+++ allone/app_ui.py
@@ -10,7 +10,7 @@ from settings_manager import load_settings, save_settings
 from updater import check_for_updates
 import backend_logic as backend
 
-__version__ = "3.2.0"
+__version__ = "3.2.1"
 
 DYMO_LABELS = {
     'Address (30252)': {'w_in': 3.5, 'h_in': 1.125},
@@ -404,3 +404,4 @@ Created by Hakan Akaslan
         self.log(log_msg)
         if success_msg: self.task_completion_popup("Success", success_msg)
         else: messagebox.showerror("Error", log_msg)
+
