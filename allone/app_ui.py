f28cc49a69f1c2c951cfec9fb47ce893ef59c567
hakanakaslanx-code <hakanakaslanx@gmail.com>
2025-10-24 11:29:01 -0400

    Update app_ui.py

--- allone/app_ui.py
+++ allone/app_ui.py
@@ -13,7 +13,7 @@ from settings_manager import load_settings, save_settings
 from updater import check_for_updates
 import backend_logic as backend
 
-__version__ = "3.5.8"
+__version__ = "3.5.9"
 
 TRANSLATIONS = {
     "en": {
