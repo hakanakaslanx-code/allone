a226c3c82c20fbb6e07425e5a10fc28599772b64
hakanakaslanx-code <hakanakaslanx@gmail.com>
2025-10-26 01:12:14 -0400

    Update app_ui.py

--- allone/app_ui.py
+++ allone/app_ui.py
@@ -15,7 +15,7 @@ from settings_manager import load_settings, save_settings
 from updater import check_for_updates
 import backend_logic as backend
 
-__version__ = "4.1.6"
+__version__ = "4.1.7"
 
 
 class CollapsiblePanel(tk.Frame):
