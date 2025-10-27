6c0c10c97e16b5d275ae4bcc030d0549102374ba
hakanakaslanx-code <hakanakaslanx@gmail.com>
2025-10-27 14:04:11 -0400

    Update app_ui.py

--- allone/app_ui.py
+++ allone/app_ui.py
@@ -18,7 +18,7 @@ from updater import check_for_updates
 import backend_logic as backend
 from wayfair_formatter import WayfairFormatter
 
-__version__ = "4.2.7"
+__version__ = "4.2.8"
 
 translations = {
     "en": {
